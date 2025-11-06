23c4bd413c6dfddb06d6fa3f9e57f2379cf89765
reletz <naufarrelzhafifabhista@gmail.com>
2025-11-06 08:53:59 +0700

    feat: add mocks for QO

---
+++ query-processor/src/test/java/com/apacy/queryprocessor/mocks/MockQueryOptimizer.java
@@ -0,0 +1,50 @@
+package com.apacy.queryprocessor.mocks;
+
+import com.apacy.common.dto.*;
+import com.apacy.common.interfaces.IQueryOptimizer;
+import java.util.List;
+
+public class MockQueryOptimizer implements IQueryOptimizer {
+    
+    @Override
+    public ParsedQuery parseQuery(String query) {
+        if (query.toLowerCase().contains("select")) {
+            return new ParsedQuery(
+                "SELECT",                                // queryType
+                List.of("users"),                        // targetTables
+                List.of("name", "email"),                // targetColumns
+                null,                                    // joinConditions
+                null,                                    // whereClause (AST)
+                "name",                                  // orderByColumn
+                false,                                   // isDescending
+                false                                    // isOptimized
+            );
+        } else if (query.toLowerCase().contains("update")) {
+            return new ParsedQuery(
+                "UPDATE",                                // queryType
+                List.of("users"),                        // targetTables
+                null,                                    // targetColumns
+                null,                                    // joinConditions
+                null,                                    // whereClause (AST)
+                null,                                    // orderByColumn
+                false,                                   // isDescending
+                false                                    // isOptimized
+            );
+        }
+        
+        // TODO: Tambahkan skenario "INSERT", "DELETE", "CREATE", dll.
+        
+        // Default (jika query tidak dikenal)
+        return null;
+    }
+
+    @Override
+    public ParsedQuery optimizeQuery(ParsedQuery query, Statistic stats) {
+        return query;
+    }
+
+    @Override
+    public double getCost(ParsedQuery query, Statistic stats) {
+        return 1;
+    }
+}
