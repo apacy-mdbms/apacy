b06a43a833e34e9f3de4ef70eaab1ea63684d23f
Rafa Abdussalam <19623187@std.stei.itb.ac.id>
2025-11-08 13:14:02 +0700

    feat : add methods serialize and deserialized

--- storage-manager/src/main/java/com/apacy/storagemanager/Serializer.java
+++ storage-manager/src/main/java/com/apacy/storagemanager/Serializer.java
@@ -1,7 +1,12 @@
 package com.apacy.storagemanager;
 
-import com.apacy.common.dto.Row;
+// import com.apacy.common.dto.Row;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Serializer handles conversion between Row objects and byte arrays for storage.
@@ -15,16 +20,67 @@ public class Serializer {
      */
     public byte[] serialize(Row row) throws IOException {
         // TODO: Implement row serialization logic
-        throw new UnsupportedOperationException("serialize not implemented yet");
+        Schema schema = row.getSchema();
+        int rowSize = schema.getFixedRowSize();
+        
+        ByteBuffer buffer = ByteBuffer.allocate(rowSize);
+
+        for (int i = 0; i < schema.getColumnCount(); i++) {
+            Column col = schema.getColumn(i);
+            Object value = row.getValue(i);
+
+            switch (col.getType()) {
+                case INTEGER:
+                    buffer.putInt((Integer) value);
+                    break;
+                case FLOAT:
+                    buffer.putFloat((Float) value);
+                    break;
+                case CHAR:
+                case VARCHAR:
+                    String strValue = (String) value;
+                    byte[] strBytes = strValue.getBytes(StandardCharsets.UTF_8);
+                    byte[] fixedBytes = new byte[col.getLength()];
+                    System.arraycopy(strBytes, 0, fixedBytes, 0, Math.min(strBytes.length, col.getLength()));
+                    buffer.put(fixedBytes);
+                    break;
+                default:
+                    throw new IOException("Tipe data tidak didukung: " + col.getType());
+            }
+        }
+        return buffer.array();
     }
     
     /**
      * Deserialize a byte array to a Row object.
      * TODO: Implement deserialization with proper type decoding and validation
      */
-    public Row deserialize(byte[] data) throws IOException {
+    public Row deserialize(byte[] data, Schema schema) throws IOException {
         // TODO: Implement row deserialization logic
-        throw new UnsupportedOperationException("deserialize not implemented yet");
+
+        ByteBuffer buffer = ByteBuffer.wrap(data);
+        List<Object> values = new ArrayList<>();
+
+        for (Column col : schema.getColumns()) {
+            switch (col.getType()) {
+                case INTEGER:
+                    values.add(buffer.getInt());
+                    break;
+                case FLOAT:
+                    values.add(buffer.getFloat());
+                    break;
+                case CHAR:
+                case VARCHAR:
+                    byte[] fixedBytes = new byte[col.getLength()];
+                    buffer.get(fixedBytes);
+                    String strValue = new String(fixedBytes, StandardCharsets.UTF_8).trim();
+                    values.add(strValue);
+                    break;
+                default:
+                    throw new IOException("Tipe data tidak didukung: " + col.getType());
+            }
+        }
+        return new Row(schema, values);
     }
     
     /**
