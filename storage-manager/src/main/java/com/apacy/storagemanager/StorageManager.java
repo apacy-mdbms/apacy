243905913868a03664cd92e88a9aa27fa5ba8149
inRiza <icon.firdaus@gmail.com>
2025-11-08 09:04:23 +0700

    feat: add init logic for read, write, and delete

--- storage-manager/src/main/java/com/apacy/storagemanager/StorageManager.java
+++ storage-manager/src/main/java/com/apacy/storagemanager/StorageManager.java
@@ -5,6 +5,12 @@ import com.apacy.common.dto.*;
 import com.apacy.common.interfaces.IStorageManager;
 import java.util.List;
 import java.util.Map;
+import java.util.Collections;
+import java.io.IOException;
+import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 
 public class StorageManager extends DBMSComponent implements IStorageManager {
 
@@ -40,7 +46,27 @@ public class StorageManager extends DBMSComponent implements IStorageManager {
         // 2. Untuk tiap blok, panggil serializer.deserialize(...)
         // 3. Filter hasilnya sesuai dataRetrieval.filterCondition
         // 4. Kembalikan List<Row>
-        throw new UnsupportedOperationException("readBlock not implemented yet");
+        try {
+            String fileName = getFileNameForTable(dataRetrieval.tableName());
+            List<Row> allRows = new ArrayList<>();
+            
+            long blockCount = blockManager.getBlockCount(fileName);
+
+            for (long blockNumber = 0; blockNumber < blockCount; blockNumber++) {
+                byte[] blockData = blockManager.readBlock(fileName, blockNumber);
+                List<Row> rowsOfBlock = serializer.deserializeBlock(blockData);
+                allRows.addAll(rowsOfBlock);
+            }
+
+            return allRows.stream()
+                .filter(row -> applyFilter(row, dataRetrieval))
+                .collect(Collectors.toList());
+
+        } catch (IOException e) {
+            System.err.println("Error reading block: " + e.getMessage());
+            return Collections.emptyList();
+        }
+        // throw new UnsupportedOperationException("readBlock not implemented yet");
     }
 
     @Override
@@ -50,7 +76,48 @@ public class StorageManager extends DBMSComponent implements IStorageManager {
         // 2. Panggil blockManager.writeBlock(...)
         // 3. Update index
         // 4. Kembalikan jumlah baris
-        throw new UnsupportedOperationException("writeBlock not implemented yet");
+        try {
+            String fileName = getFileNameForTable(dataWrite.tableName());
+
+            long blockCount = blockManager.getBlockCount(fileName);
+            byte[] blockData;
+            long targetBlockNumber;
+
+            if (blockCount == 0) {
+                // buat blok baru
+                blockData = serializer.initializeNewBlock();
+                targetBlockNumber = 0;
+            } else {
+                // ambil blok terakhir
+                targetBlockNumber = blockCount - 1;
+                blockData = blockManager.readBlock(fileName, targetBlockNumber);
+            }
+
+            try {
+                blockData = serializer.packRowToBlock(blockData, dataWrite.newData());
+                blockManager.writeBlock(fileName, targetBlockNumber, blockData);
+                blockManager.flush();
+                // TODO: index update kondisional
+                // TODO: stats update
+                return 1;
+            } catch (IOException ex) {
+                if (blockCount > 0) {
+                    blockData = serializer.initializeNewBlock();
+                    blockData = serializer.packRowToBlock(blockData, dataWrite.newData());
+                    long newBlockNumber = blockManager.appendBlock(fileName, blockData);
+                    // TODO: update index dengan dengan newblock
+                    // TODO: update stats dengan newblock
+                    System.out.println("Appended new block number: " + newBlockNumber);
+                    blockManager.flush();
+                    return 1;
+                }
+                throw ex;
+            }
+        } catch (IOException e) {
+            System.err.println("Error writing block: " + e.getMessage());
+            return 0;
+        }
+        // throw new UnsupportedOperationException("writeBlock not implemented yet");
     }
 
     @Override
@@ -60,7 +127,50 @@ public class StorageManager extends DBMSComponent implements IStorageManager {
         // 2. Hapus/modifikasi bloknya
         // 3. Update index
         // 4. Kembalikan jumlah baris
-        throw new UnsupportedOperationException("deleteBlock not implemented yet");
+        try {
+            String fileName = getFileNameForTable(dataDeletion.tableName());
+            int deletedCount = 0;
+            List<Row> deletedRows = new ArrayList<>();
+
+            long blockCount = blockManager.getBlockCount(fileName);
+
+            for (long blockNumber = 0; blockNumber < blockCount; blockNumber++) {
+                byte[] blockData = blockManager.readBlock(fileName, blockNumber);
+                List<Row> rowsOfBlock = serializer.deserializeBlock(blockData);
+
+                List<Row> remainingRows = new ArrayList<>();
+                for (Row row : rowsOfBlock) {
+                    if (matchesDeleteCondition(row, dataDeletion)) {
+                        deletedCount = deletedCount + 1;
+                        deletedRows.add(row); // ini buat update index nanti
+                    } else {
+                        remainingRows.add(row);
+                    }
+                }
+
+                if (!remainingRows.isEmpty()) {
+                    byte[] newBlockData = serializer.initializeNewBlock();
+                    for (Row row : remainingRows) {
+                        newBlockData = serializer.packRowToBlock(newBlockData, row);
+                    }
+                    blockManager.writeBlock(fileName, blockNumber, newBlockData);
+                } else if (rowsOfBlock.size() > 0) {
+                    blockManager.writeBlock(fileName, blockNumber, serializer.initializeNewBlock());
+                }
+            }
+
+            blockManager.flush();
+            // TODO: update index
+            // TODO: update stats
+            // if (deletedCount > 0) {
+
+            // }
+            return deletedCount;
+        } catch (IOException e) {
+            System.err.println("Error deleting block: " + e.getMessage());
+            return 0;
+        }
+        // throw new UnsupportedOperationException("deleteBlock not implemented yet");
     }
 
     @Override
@@ -77,5 +187,39 @@ public class StorageManager extends DBMSComponent implements IStorageManager {
         // 1. Panggil statsCollector.collectStats()
         // 2. Kembalikan map dari String, Statistic (Nama tabel dan statistiknya)
         throw new UnsupportedOperationException("getStats not implemented yet");
+    }
+
+    /** 
+     * Konversi nama tabel ke nama file
+     * format ditentukannya "tableName_table.dat"
+     * @param tableName as nama tablenya
+     * @return fileName as nama filenya 
+     */
+    private String getFileNameForTable(String tableName) {
+        return tableName + "_table.dat";
+    }
+
+    /**
+     * Apply filter condition ke sebuah row
+     * @param row as row yang mau difilter
+     * @param dataRetrieval as data yang ingin dicek
+     * @return true jika lolos filter
+     */
+    private boolean applyFilter(Row row, DataRetrieval dataRetrieval) {
+        if (dataRetrieval.filterCondition() == null) {
+            return true;
+        }
+        // TODO: parse sama evaluasi filterCondition
+        // TODO: tergantung tipe/struktur nanti di query processing
+        // masih dummy dan dianngap lolos selain null..
+        return true;
+    }
+
+    private boolean matchesDeleteCondition(Row row, DataDeletion dataDeletion) {
+        if (dataDeletion.filterCondition() == null) {
+            return true;
+        }
+        // TODO: parse sama evaluasi filterCondition
+        return false;
     }
 }
