58b8460400579f77cab45aa3d7fa2c3f214bee5e
JasonFernandoo <j450n.fernando@gmail.com>
2025-11-13 14:28:51 +0700

    refactor: enhance SelectParser with improved join condition handling and expression node integration

--- query-optimizer/src/main/java/com/apacy/queryoptimizer/parser/SelectParser.java
+++ query-optimizer/src/main/java/com/apacy/queryoptimizer/parser/SelectParser.java
@@ -5,12 +5,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.apacy.common.dto.ParsedQuery;
+import com.apacy.queryoptimizer.ast.expression.ColumnFactor;
+import com.apacy.queryoptimizer.ast.expression.ExpressionNode;
+import com.apacy.queryoptimizer.ast.expression.LiteralFactor;
+import com.apacy.queryoptimizer.ast.expression.TermNode;
 import com.apacy.queryoptimizer.ast.join.JoinConditionNode;
 import com.apacy.queryoptimizer.ast.join.JoinOperand;
 import com.apacy.queryoptimizer.ast.join.TableNode;
-import com.apacy.queryoptimizer.ast.where.BinaryConditionNode;
 import com.apacy.queryoptimizer.ast.where.ComparisonConditionNode;
-import com.apacy.queryoptimizer.ast.where.UnaryConditionNode;
 import com.apacy.queryoptimizer.ast.where.WhereConditionNode;
 
 public class SelectParser extends AbstractParser {
@@ -21,8 +23,6 @@ public class SelectParser extends AbstractParser {
 
     @Override
     public ParsedQuery parse() throws ParseException {
-        return null;
-        /*
         consume(TokenType.SELECT);
 
         List<String> targetColumns = new ArrayList<>();
@@ -50,12 +50,23 @@ public class SelectParser extends AbstractParser {
             targetTables.add(rightTableToken.getValue());
 
             consume(TokenType.ON);
-            String leftOperand = consume(TokenType.IDENTIFIER).getValue();
+            String leftOp = consume(TokenType.IDENTIFIER).getValue();
             String operator = consume(TokenType.OPERATOR).getValue();
-            Token rightOpToken = consume(peek().getType());
-            String rightOperand = rightOpToken.getValue();
+            Token rightOpToken = peek();
+            ExpressionNode leftExpr = new ExpressionNode(new TermNode(new ColumnFactor(leftOp), null), null);
+            ExpressionNode rightExpr;
+            if (rightOpToken.getType() == TokenType.IDENTIFIER) {
+                position++;
+                rightExpr = new ExpressionNode(new TermNode(new ColumnFactor(rightOpToken.getValue()), null), null);
+            } else if (rightOpToken.getType() == TokenType.NUMBER_LITERAL) {
+                position++;
+                rightExpr = new ExpressionNode(new TermNode(new LiteralFactor(parseNumberLiteral(rightOpToken.getValue())), null), null);
+            } else {
+                position++;
+                rightExpr = new ExpressionNode(new TermNode(new ColumnFactor(rightOpToken.getValue()), null), null);
+            }
 
-            ComparisonConditionNode condition = new ComparisonConditionNode(leftOperand, operator, rightOperand);
+            ComparisonConditionNode condition = new ComparisonConditionNode(leftExpr, operator, rightExpr);
             JoinConditionNode thisJoin = new JoinConditionNode("INNER", leftTable, rightTable, condition);
 
             if (joinAst == null) {
@@ -87,15 +98,13 @@ public class SelectParser extends AbstractParser {
 
         Object joinConditions = joinAst;
         Object whereClause = where;
-
-        return new ParsedQuery("SELECT", targetTables, targetColumns, joinConditions, whereClause, orderBy, isDesc, false);
-        */
+    return new ParsedQuery("SELECT", targetTables, targetColumns,
+                (List<Object>) null, joinConditions, whereClause,
+                orderBy, isDesc, false);
     };
 
     @Override
     public boolean validate() {
-        return false;
-        /*
         try {
             int savedPos = position;
             if (!match(TokenType.SELECT)) return false;
@@ -148,91 +157,5 @@ public class SelectParser extends AbstractParser {
         } catch (Exception e) {
             return false;
         }
-        */
     }
-
-    /*
-    private WhereConditionNode parseWhereExpression() {
-        return parseOr();
-    }
-
-    private WhereConditionNode parseOr() {
-        WhereConditionNode left = parseAnd();
-        while (match(TokenType.OR)) {
-            WhereConditionNode right = parseAnd();
-            left = new BinaryConditionNode(left, "OR", right);
-        }
-        return left;
-    }
-
-    private WhereConditionNode parseAnd() {
-        WhereConditionNode left = parseUnary();
-        while (match(TokenType.AND)) {
-            WhereConditionNode right = parseUnary();
-            left = new BinaryConditionNode(left, "AND", right);
-        }
-        return left;
-    }
-
-    private WhereConditionNode parseUnary() {
-        if (match(TokenType.NOT)) {
-            WhereConditionNode operand = parsePrimary();
-            return new UnaryConditionNode("NOT", operand);
-        }
-        return parsePrimary();
-    }
-
-    private WhereConditionNode parsePrimary() {
-        if (match(TokenType.LPARENTHESIS)) {
-            WhereConditionNode inner = parseWhereExpression();
-            consume(TokenType.RPARENTHESIS);
-            return inner;
-        }
-
-        String left = consume(TokenType.IDENTIFIER).getValue();
-        String operator = consume(TokenType.OPERATOR).getValue();
-        Token rightTok = peek();
-        if (rightTok.getType() == TokenType.NUMBER_LITERAL) {
-            position++;
-            return new ComparisonConditionNode(left, operator, parseNumberLiteral(rightTok.getValue()));
-        } else if (rightTok.getType() == TokenType.STRING_LITERAL || rightTok.getType() == TokenType.IDENTIFIER) {
-            position++;
-            return new ComparisonConditionNode(left, operator, rightTok.getValue());
-        } else {
-            throw new RuntimeException("Invalid where clause token: " + rightTok.getType());
-        }
-    }
-
-    private Object parseNumberLiteral(String value) {
-        try { return Integer.parseInt(value); } catch (NumberFormatException e) {}
-        try { return Double.parseDouble(value); } catch (NumberFormatException e) {}
-        return value;
-    }
-
-    private boolean validateWhere() {
-        int depth = 0;
-        boolean expectOperand = true;
-        while (position < tokens.size()) {
-            Token t = peek();
-            if (t.getType() == TokenType.IDENTIFIER) {
-                position++;
-                if (peek().getType() != TokenType.OPERATOR) return false;
-                position++;
-                if (peek().getType() != TokenType.NUMBER_LITERAL && peek().getType() != TokenType.IDENTIFIER && peek().getType() != TokenType.STRING_LITERAL) return false;
-                position++;
-                expectOperand = false;
-            } else if (t.getType() == TokenType.AND || t.getType() == TokenType.OR) {
-                position++;
-                expectOperand = true;
-            } else if (t.getType() == TokenType.LPARENTHESIS) {
-                depth++; position++; expectOperand = true;
-            } else if (t.getType() == TokenType.RPARENTHESIS) {
-                depth--; position++; expectOperand = false;
-                if (depth < 0) return false;
-            } else break;
-        }
-        return depth == 0 && !expectOperand;
-    }
-
-    */
 }
