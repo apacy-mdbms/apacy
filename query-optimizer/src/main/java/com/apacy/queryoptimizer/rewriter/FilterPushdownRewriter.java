d2cec1e91c91211370aa79a480b705d996e7e90a
L4mbads <rizacal.mamen@gmail.com>
2025-11-20 20:38:52 +0700

    fix: always push filter below projectino

--- query-optimizer/src/main/java/com/apacy/queryoptimizer/rewriter/FilterPushdownRewriter.java
+++ query-optimizer/src/main/java/com/apacy/queryoptimizer/rewriter/FilterPushdownRewriter.java
@@ -7,7 +7,6 @@ import com.apacy.common.dto.plan.FilterNode;
 import com.apacy.common.dto.plan.PlanNode;
 import com.apacy.common.dto.plan.ProjectNode;
 import com.apacy.queryoptimizer.CostEstimator;
-import com.apacy.queryoptimizer.ast.where.WhereConditionNode;
 
 // push down filter node
 public class FilterPushdownRewriter extends PlanRewriter {
@@ -23,20 +22,14 @@ public class FilterPushdownRewriter extends PlanRewriter {
 
         // Try pushdown over PROJECT
         if (rewrittenChild instanceof ProjectNode p) {
-            if (canPredicateBePushed((WhereConditionNode)node.predicate(), p)) {
-                return new ProjectNode(
-                    new FilterNode(p.child(), node.predicate()),
-                    p.columns()
-                );
-            }
+            return new ProjectNode(
+                new FilterNode(p.child(), node.predicate()),
+                p.columns()
+            );
         }
 
         if (child == rewrittenChild) return node;
         return new FilterNode(rewrittenChild, node.predicate());
     }
 
-    protected boolean canPredicateBePushed(WhereConditionNode predicate, ProjectNode p) {
-        // TODO: return true if attribute exists in child
-        return true;
-    }
 }
