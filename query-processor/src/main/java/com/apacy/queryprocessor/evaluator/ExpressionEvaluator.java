17655d65275a50906678af9f4c9518fe2d7f54f1
Muhammad Kinan Arkansyaddad <mkinanarkansyaddad@gmail.com>
2025-12-02 22:04:34 +0700

    feat: add ExpressionEvaluator utility for AST evaluation

---
+++ query-processor/src/main/java/com/apacy/queryprocessor/evaluator/ExpressionEvaluator.java
@@ -0,0 +1,58 @@
+package com.apacy.queryprocessor.evaluator;
+
+import com.apacy.queryoptimizer.ast.expression.ColumnFactor;
+import com.apacy.queryoptimizer.ast.expression.ExpressionNode;
+import com.apacy.queryoptimizer.ast.expression.FactorNode;
+import com.apacy.queryoptimizer.ast.expression.LiteralFactor;
+import com.apacy.queryoptimizer.ast.expression.TermNode;
+
+/**
+ * Utility class to evaluate AST ExpressionNodes.
+ */
+public class ExpressionEvaluator {
+    public static Object evaluate(Object node) {
+        if (node == null) return null;
+
+        if (node instanceof String || node instanceof Number || node instanceof Boolean) {
+            return node;
+        }
+
+        if (node instanceof ExpressionNode expr) {
+            return evaluateExpression(expr);
+        }
+        if (node instanceof TermNode term) {
+            return evaluateTerm(term);
+        }
+        if (node instanceof FactorNode factor) {
+            return evaluateFactor(factor);
+        }
+
+        return node;
+    }
+
+    private static Object evaluateExpression(ExpressionNode expr) {
+        if (expr == null) return null;
+        return evaluateTerm(expr.term());
+    }
+
+    private static Object evaluateTerm(TermNode term) {
+        if (term == null) return null;
+        return evaluateFactor(term.factor());
+    }
+
+    private static Object evaluateFactor(FactorNode factor) {
+        if (factor == null) return null;
+
+        if (factor instanceof LiteralFactor lit) {
+            return lit.value();
+        }
+        if (factor instanceof ExpressionNode nestedExpr) {
+            return evaluateExpression(nestedExpr);
+        }
+        if (factor instanceof ColumnFactor col) {
+            throw new UnsupportedOperationException("Column reference '" + col.columnName() + "' not supported in this value context.");
+        }
+
+        return null;
+    }
+}
