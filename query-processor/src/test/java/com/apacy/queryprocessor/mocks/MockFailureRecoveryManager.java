8d469f06a57c68d920a7a997ba96274ae2c79ee2
reletz <naufarrelzhafifabhista@gmail.com>
2025-12-03 19:13:40 +0700

    chore: update mock

--- query-processor/src/test/java/com/apacy/queryprocessor/mocks/MockFailureRecoveryManager.java
+++ query-processor/src/test/java/com/apacy/queryprocessor/mocks/MockFailureRecoveryManager.java
@@ -1,15 +1,14 @@
-package com.apacy.queryprocessor.mocks;
-
-import com.apacy.common.dto.ExecutionResult;
-import com.apacy.common.dto.RecoveryCriteria;
-import com.apacy.common.dto.Row;
-import com.apacy.common.interfaces.IFailureRecoveryManager;
-
 public class MockFailureRecoveryManager implements IFailureRecoveryManager {
 
     @Override
     public void writeLog(ExecutionResult info) {
-        System.out.println("[MOCK-FRM] writeLog: Tx " + info.transactionId() + " | Op: " + info.operation() + " | Status: " + info.success());
+        System.out.println("[MOCK-FRM] writeLog: Tx " + info.transactionId() + " | Op: " + info.operation()
+                + " | Status: " + info.success());
+    }
+
+    @Override
+    public void writeTransactionLog(int transactionId, String lifecycleEvent) {
+        System.out.println("[MOCK-FRM] writeTransactionLog: Tx " + transactionId + " | Event: " + lifecycleEvent);
     }
 
     @Override
@@ -19,16 +18,7 @@ public class MockFailureRecoveryManager implements IFailureRecoveryManager {
 
     @Override
     public void recover(RecoveryCriteria criteria) {
-        System.out.println("[MOCK-FRM] RECOVER dipanggil. Type: " + criteria.recoveryType() + ", TxId: " + criteria.transactionId());
-    }
-
-    @Override
-    public void writeDataLog(String transactionId, String operation, String tableName, Row dataBefore, Row dataAfter) {
-        return;
-    }
-
-    @Override
-    public void writeTransactionLog(int transactionId, String lifecycleEvent) {
-        return;
+        System.out.println("[MOCK-FRM] RECOVER dipanggil. Type: " + criteria.recoveryType() + ", TxId: "
+                + criteria.transactionId());
     }
 }
