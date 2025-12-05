2893664d4e2500daec0be1b59248c3f06a47f2c3
Muhammad Kinan Arkansyaddad <mkinanarkansyaddad@gmail.com>
2025-12-05 08:42:56 +0700

    feat: add primary key and foreign key validation when insert new row

--- query-processor/src/main/java/com/apacy/queryprocessor/execution/ModifyOperator.java
+++ query-processor/src/main/java/com/apacy/queryprocessor/execution/ModifyOperator.java
@@ -6,8 +6,12 @@ import java.util.List;
 import java.util.Map;
 
 import com.apacy.common.dto.DataDeletion;
+import com.apacy.common.dto.DataRetrieval;
 import com.apacy.common.dto.DataWrite;
+import com.apacy.common.dto.ForeignKeySchema;
+import com.apacy.common.dto.IndexSchema;
 import com.apacy.common.dto.Row;
+import com.apacy.common.dto.Schema;
 import com.apacy.common.dto.plan.ModifyNode;
 import com.apacy.common.interfaces.IConcurrencyControlManager;
 import com.apacy.common.interfaces.IFailureRecoveryManager;
@@ -112,11 +116,85 @@ public class ModifyOperator implements Operator {
 
         Row newRow = new Row(dataMap);
 
+        validatePrimaryKey(newRow, node.targetTable());
+
+        validateInsert(newRow, node.targetTable());
+
         frm.writeDataLog(String.valueOf(txId), "INSERT", node.targetTable(), null, newRow);
 
         DataWrite dw = new DataWrite(node.targetTable(), newRow, null);
         affectedRows = sm.writeBlock(dw);
     }
+
+    private void validatePrimaryKey(Row newRow, String tableName) {
+    Schema schema = sm.getSchema(tableName);
+    if (schema == null) return;
+
+    String pkColumnName = null;
+    String prefix = "pk_" + tableName + "_";
+
+    for (IndexSchema idx : schema.indexes()) {
+        if (idx.indexName().startsWith(prefix)) {
+            pkColumnName = idx.columnName();
+            break; 
+        }
+    }
+
+    if (pkColumnName == null) return;
+
+    Object pkValue = newRow.get(pkColumnName);
+
+    if (pkValue == null) {
+        throw new RuntimeException("Primary Key Violation: Column '" + pkColumnName + "' cannot be NULL.");
+    }
+
+    String filter = pkColumnName + "='" + pkValue + "'";
+    
+    DataRetrieval checkPK = new com.apacy.common.dto.DataRetrieval(
+        tableName,
+        java.util.List.of(pkColumnName),
+        filter,
+        true
+    );
+
+    List<Row> results = sm.readBlock(checkPK);
+
+    if (!results.isEmpty()) {
+        throw new RuntimeException("Primary Key Violation: Duplicate entry '" + pkValue + "' for key '" + pkColumnName + "'.");
+    }
+}
+
+    private void validateInsert(Row newRow, String tableName) {
+    Schema schema = sm.getSchema(tableName);
+
+    System.out.println(schema.getForeignKeys());
+    
+    if (schema == null || schema.getForeignKeys() == null) return;
+
+    for (ForeignKeySchema fk : schema.getForeignKeys()) {
+        
+        Object childValue = newRow.get(fk.columnName());
+
+        if (childValue == null) continue;
+
+        String filter = fk.referenceColumn() + "='" + childValue + "'";
+        
+        DataRetrieval checkParent = new DataRetrieval(
+            fk.referenceTable(),
+            java.util.List.of(fk.referenceColumn()),
+            filter,
+            true
+        );
+
+        List<Row> results = sm.readBlock(checkParent);
+
+        if (results.isEmpty()) {
+            throw new RuntimeException("Integrity Constraint Violation: The value '" + childValue + 
+                "' for column '" + fk.columnName() + "' does not exist in referenced table '" + 
+                fk.referenceTable() + "'.");
+        }
+    }
+}
 
     private void doDelete() {
         if (child == null) {
