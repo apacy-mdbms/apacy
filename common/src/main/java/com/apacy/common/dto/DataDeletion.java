74da90666aefec3d76bb1f6ba8762be19acd0fe4
iammadsfq <13523135@std.stei.itb.ac.id>
2025-12-06 16:05:36 +0700

    refactor: BREAKING change filterCondition of dataretrieval etc to a WhereConditionNode

--- common/src/main/java/com/apacy/common/dto/DataDeletion.java
+++ common/src/main/java/com/apacy/common/dto/DataDeletion.java
@@ -1,10 +1,12 @@
 package com.apacy.common.dto;
 
+import com.apacy.common.dto.ast.where.WhereConditionNode;
+
 /**
  * Dibuat oleh QP, dikonsumsi oleh SM.
  * Instruksi spesifik ke SM tentang data apa yang harus DIHAPUS.
  */
 public record DataDeletion(
     String tableName,
-    Object filterCondition // Sebaiknya merujuk ke class/record internal SM
+    WhereConditionNode filterCondition // Sebaiknya merujuk ke class/record internal SM
 ) {}
